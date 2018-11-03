b0f9b8c0278e2d04894ceac9532f2f0ef8095fc7
Ryllari <ryllari.marques@lit.ifce.edu.br>
2018-11-03 15:48:17 -0300

    Create BlogPost model and migration

--- djangoapp/app/models.py
+++ djangoapp/app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class BlogPost(models.Model):
+    """
+    This class represents a BlogPost
+    A BlogPost contains information about a Post on Blog
+
+    :cvar str title: post's title
+    :cvar str short_text: post's short_text
+    :cvar datetime posted_at: post's timestamp posted
+    """
+    title = models.TextField()
+    short_text = models.TextField()
+    posted_at = models.DateTimeField()
